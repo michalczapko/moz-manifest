7847d0d342089af6defc20e2d34e07541659e1de
Michał Czapko <michal@czapko.pl>
2013-09-03 11:51:56 +0200

    Add original manifest

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -2,4 +2,8 @@ class ManifestController < ApplicationController
   def index
     send_file 'public/manifest.webapp', :type => 'application/x-web-app-manifest+json'
   end
+
+  def original
+    send_file 'public/original.manifest.webapp', :type => 'application/x-web-app-manifest+json'
+  end
 end
